40f2b53b35673db2f62ef122339f57712addb9e6
vkku2016 <vk733361@gmail.com>
2025-08-24 20:33:25 +0530

    traction done

--- traction.java
+++ traction.java
@@ -8,15 +8,13 @@ class A{
 	public static void main (String arg[]){
 		System.out.println(a+b);
 System.out.println("welcome git rep");
-<<<<<<< HEAD
-String s="vishu";
 
-System.out.println("************************************");
-=======
+String s="vishu";
+System.out.println("************************************
 System.out.println("------------------------------");
 String s1="hello";
 String s2 ="welcome";
->>>>>>> be798858dea490dd7f15c032e8be502b35fe70df
+
 
 	}
 }
